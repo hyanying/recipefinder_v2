5818de4da96537808e2bcb7572179bb2a79b9fdf
hyanying <yan.and.ying@gmail.com>
2016-09-13 05:49:07 +0100

    add conditional statement if no recipe details found

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,12 +6,12 @@ class RecipesController < ApplicationController
 
   	@search_term = params[:looking_for] || generateFood()
   	@recipes = Recipe.for(@search_term)
-  	@recipeDetails = Recipe.getDetails()
+  	#@recipeDetails = Recipe.getDetails()
   	
   end
 
   def modal
-  	@recipeDetails = Recipe.getDetails()
+  	#@recipeDetails = Recipe.getDetails()
   end	
 	
 end
